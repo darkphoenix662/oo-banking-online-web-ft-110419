3ec8f5b21d45df1c079f8c6ed173479efb7717a8
Sean Good <tree662@protonmail.com>
2019-12-03 15:57:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # your code here
+  attr_accessor :amount, :sender, :receiver
+  attr_accessor :status 
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount 
+  end 
+  
+  def valid?
+    sender.valid? == receiver.valid?
+  end 
+  
+  def execute_transaction
+    
+  end
 end
